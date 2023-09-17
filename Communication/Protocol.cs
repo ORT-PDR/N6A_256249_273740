cdb16f6331bc81e5364de43327133798e99a4891
Juan Bervejillo <juaberve@gmail.com>
2023-09-17 17:10:13 -0300

    Modify Protocol

--- Communication/Protocol.cs
+++ Communication/Protocol.cs
@@ -1,76 +1,95 @@
 using System;
+using System.IO;
+using System.Net.Sockets;
 using System.Text;
-using System.Text.Json;
 
 namespace Communication
 {
-    public class Protocol
+    public static class Protocol
     {
-        public static class Commands
+        private static TcpClient client;
+        private static NetworkStream stream;
+
+        static Protocol()
         {
-            public const string Authenticate = "AUTH";
-            public const string PublishProduct = "PUBL";
-            public const string PurchaseProduct = "COMP";
-        }
-        
-        private class ProtocolFrame
-        {
-            public string Header { get; set; }
-            public string Body { get; set; }
-        }
-        
-        public static string BuildRequest(string command, object data)
-        {
-            var requestFrame = new ProtocolFrame
-            {
-                Header = command,
-                Body = JsonSerializer.Serialize(data)
-            };
-            return JsonSerializer.Serialize(requestFrame);
+            client = new TcpClient();
         }
 
-        public static string BuildResponse(string command, object data)
-        {
-            var responseFrame = new ProtocolFrame
-            {
-                Header = command,
-                Body = JsonSerializer.Serialize(data)
-            };
-            return JsonSerializer.Serialize(responseFrame);
-        }
-        
-        public static (string command, T data) ParseRequest<T>(string request)
+        public static void Connect(string ipAddress, int port)
         {
             try
             {
-                var requestData = JsonSerializer.Deserialize<ProtocolFrame>(request);
-                return (requestData.Header, JsonSerializer.Deserialize<T>(requestData.Body));
-            }
-            catch (JsonException ex)
-            {
-                throw new ProtocolException("Error al analizar la solicitud JSON.", ex);
+                client.Connect(ipAddress, port);
+                stream = client.GetStream();
             }
             catch (Exception ex)
             {
-                throw new ProtocolException("Error al analizar la solicitud.", ex);
+                throw new ProtocolException("Connection error: " + ex.Message);
             }
         }
 
-        public static (string command, T data) ParseResponse<T>(string response)
+        public static void Disconnect()
         {
             try
             {
-                var responseData = JsonSerializer.Deserialize<ProtocolFrame>(response);
-                return (responseData.Header, JsonSerializer.Deserialize<T>(responseData.Body));
-            }
-            catch (JsonException ex)
-            {
-                throw new ProtocolException("Error al analizar la respuesta JSON.", ex);
+                stream.Close();
+                client.Close();
             }
             catch (Exception ex)
             {
-                throw new ProtocolException("Error al analizar la respuesta.", ex);
+                throw new ProtocolException("Disconnection error: " + ex.Message);
             }
+        }
+
+        public static void SendRequest(string header, string command, string data)
+        {
+            try
+            {
+                string message = $"{header}:{command}:{data.Length}:{data}";
+                byte[] messageBytes = Encoding.ASCII.GetBytes(message);
+
+                stream.Write(BitConverter.GetBytes(messageBytes.Length), 0, 4);
+                stream.Write(messageBytes, 0, messageBytes.Length);
+            }
+            catch (Exception ex)
+            {
+                throw new ProtocolException("Error sending request: " + ex.Message);
+            }
+        }
+
+        public static string ReceiveResponse()
+        {
+            try
+            {
+                byte[] lengthBuffer = new byte[4];
+                stream.Read(lengthBuffer, 0, 4);
+                int messageLength = BitConverter.ToInt32(lengthBuffer, 0);
+
+                byte[] messageBytes = new byte[messageLength];
+                stream.Read(messageBytes, 0, messageLength);
+
+                string message = Encoding.ASCII.GetString(messageBytes);
+
+                string[] parts = message.Split(':');
+                if (parts.Length == 4)
+                {
+                    string header = parts[0];
+                    string command = parts[1];
+                    int dataLength = int.Parse(parts[2]);
+                    string data = parts[3];
+
+                    if (data.Length == dataLength)
+                    {
+                        return data;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new ProtocolException("Error receiving response: " + ex.Message);
+            }
+
+            return null;
         }
     }
 }
